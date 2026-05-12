aebb1cfbe1546240c381ab41affb2daffe6acaa2
Juan Chicharro <juanchicharro2007@gmail.com>
2026-05-12 22:25:31 +0200

    Dia 5

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ if (boton != null) {
 if (localStorage.getItem("modo") == "oscuro"){
     document.body.classList.add("oscuro");
 }
+
+const estudios = [];
+const div = document.getElementById("div-estudios");
+function agregarEstudio() {
+    const tituloo = document.getElementById("titulo").value;
+    const descripcionn = document.getElementById("descripcion").value;
+    const nuevoEstudio = {
+        titulo: tituloo,
+        descripcion: descripcionn
+    }
+    estudios.push(nuevoEstudio);
+    mostrarEstudio();
+    document.getElementById("titulo").value = "";
+    document.getElementById("descripcion").value = "";
+}
+function mostrarEstudio() {
+    div.innerHTML = "";
+    estudios.forEach(estudio => {
+        var artic = document.createElement("article");
+        artic.classList.add("estu");
+        artic.innerHTML = `
+                                <h3>${estudio.titulo}</h3>
+                                <p>${estudio.descripcion}</p>
+                            `;
+        div.appendChild(artic);
+    });
+}
